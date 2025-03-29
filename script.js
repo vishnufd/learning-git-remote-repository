1ec761b3a6a2fc113d2b1e4468fbc67008189440
Vishnu Anil <vishnuanilfrontend@gmail.com>
2025-03-29 11:19:40 +0530

    update function

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const heading = 'Landing Page'
 for (let i = 0; i <= 5; i++) {
   console.log(i, heading)
 }
+<<<<<<< Updated upstream
+=======
+
+function generateRandomNumbers() {
+
+}
+>>>>>>> Stashed changes
